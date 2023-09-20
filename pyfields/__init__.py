d392687526f587a494ce9bc4d0e4aa3656d75548
vivekkumar15 <kmr.vivek15@gmail.com>
2023-09-20 17:11:04 +0530

    setup3

--- pyfields/__init__.py
+++ pyfields/__init__.py
@@ -16,9 +16,7 @@ try:
     from ._version import version as __version__
 except ImportError:
     # Source mode : use setuptools_scm to get the current version from src using git
-    from setuptools_scm import get_version as _gv
-    from os import path as _path
-    __version__ = _gv(_path.join(_path.dirname(__file__), _path.pardir))
+    __version__ = "1.7.1.dev9+gc08cfb3"
 
 __all__ = [
     '__version__',
